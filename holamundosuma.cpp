5667a89b4b9d421c8ff89b18196662e088302b93
juveian <juvetuzos25@gmail.com>
2017-12-12 22:07:03 -0600

    FUNCION SUMA

---
+++ holamundosuma.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hola Mundo" << endl;
+
+	int a,b,suma;
+
+	cout<<"\n ingrese numero a:";
+
+	cin>>a;
+
+	cout<<"\n ingrese numero b:";
+
+	cin>>b;
+
+	suma=a+b;
+
+	cout<<"\n la suma de "<<a<<"+"<<b<<" es:"<<suma;
+
+
+    return 0;
+
+}
